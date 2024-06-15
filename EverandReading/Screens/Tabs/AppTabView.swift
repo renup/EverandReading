23e5afa027eb25e7624c618ddd3314ac815daa72
Renu Punjabi <rpunjabi12@gmail.com>
2024-06-15 06:11:16 -0700

    Tab taps are working

--- EverandReading/Screens/Tabs/AppTabView.swift
+++ EverandReading/Screens/Tabs/AppTabView.swift
@@ -7,20 +7,27 @@
 
 import SwiftUI
 
+@Observable
+final class AppScreenController: Sendable {
+    var selection: AppTab = .topCharts
+}
+
 struct AppTabView: View {
     @State private var appScreenController = AppScreenController()
 
     var body: some View {
-        TabView(selection: Bindable( appScreenController).selection)  {
+        TabView(selection: $appScreenController.selection) {
             ForEach(AppTab.allCases) { screen in
                 screen.destination
-                    .tabItem {  screen.tabLabel }
-                    .tag(screen as AppTab)
+                    .tabItem { screen.tabLabel }
+                    .tag(screen)
             }
         }
+        .environment(appScreenController)
     }
 }
 
 #Preview {
     AppTabView()
+        .environment(AppScreenController())
 }
